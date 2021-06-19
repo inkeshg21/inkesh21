a69a3e3c14f4a92a382fa72be08cc505cce4b7d8
inkesh gharti <inkeshg@gmail.com>
2021-06-19 19:05:36 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var m="inkesh";
+console.log(m);
+function f1(){
+    f1=document.getElementById("input").value;
+    console.log(f1);
+    document.getElementById("output").innerHTML=f1;
+}
+function f2(){
+    f3=parseInt(document.getElementById("inputs").value);
+    console.log(f3);
+    f4=parseInt(document.getElementById("inputss").value);
+    document.getElementById("outputs").innerHTML=f3+f4;
+}
